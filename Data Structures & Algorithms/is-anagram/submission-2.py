785aab81aa832b3fa980bbbfc3632f1a9af56ed3
wiseboat11 <zuhairmohammedabdulrahim@gmail.com>
2026-05-30 20:34:38 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq1 = {}
+        freq2 = {}
+        for s1,s2 in zip_longest(s,t):
+            freq1[s1] = freq1.get(s1,0)+1
+            freq2[s2] = freq2.get(s2,0)+1
+        return freq1==freq2
+
+        
